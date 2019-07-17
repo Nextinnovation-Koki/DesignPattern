863908bd0fd5865b587d6e9fe971c793145fa334
Koki Nakamura <koki.nakamura@nextinnovation-inc.co.jp>
2019-07-17 15:01:58 +0900

    Initial Commit

---
+++ DesignPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignPattern
+//
+//  Created by 中村 洸貴 on 2019/07/17.
+//  Copyright © 2019 中村 洸貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
